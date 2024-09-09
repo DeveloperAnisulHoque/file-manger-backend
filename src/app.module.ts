09fe943477541e2e62d19ac2afbeeb019a9e0389
Md Anisul hoque <anisulhoque587@gmail.com>
2024-09-09 23:35:53 +0600

    Added res module and set up PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FileModule } from './file/file.module';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env'] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        database: configService.get('DATABASE_NAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        synchronize: configService.get<boolean>('DATABASE_SYNC'),
+        logging: configService.get<boolean>('DATABASE_LOGGING'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      }),
+    }),
+    FileModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
